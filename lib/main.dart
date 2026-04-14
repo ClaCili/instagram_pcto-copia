4340ea5ebe031c93d4d2f641ba2c23ec6f107bdc
ClaCIli <claacili00@gmail.com>
2026-04-14 09:03:34 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 255, 0, 0)),
+      ),
+      home: Container(
+        color: Colors.white,
+        child: SafeArea(
+          bottom: true,
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: SvgPicture.asset("assets/logo.svg")),
+                SizedBox(height: 40),
+                Container(
+                  height: 44,
+                  width: 307,
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: const Color.fromARGB(255, 55, 151, 239),
+                  ),
+                ),
+              ],
+            ),
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
